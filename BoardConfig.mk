faf4b41fe627478d874af4442b9fd8194a813e3a
bengris32 <bengris32@protonmail.ch>
2022-12-10 10:12:13 +0000

    Q25: Initial skeleton tree
    [electimon: Modification for python based extract-utils]
    Change-Id: Ib039cc448a3da96806d5259a2ea4f578e4f42189
    Signed-off-by: bengris32 <bengris32@protonmail.ch>
    Signed-off-by: Stella Diaz <electi@yzu.moe>

---
+++ BoardConfig.mk
@@ -0,0 +1,9 @@
+#
+# SPDX-FileCopyrightText: The LineageOS Project
+# SPDX-License-Identifier: Apache-2.0
+#
+
+DEVICE_PATH := device/xelex/Q25
+
+# Inherit the proprietary files
+include vendor/xelex/Q25/BoardConfigVendor.mk
---
+++ README.md
@@ -0,0 +1,8 @@
+# Android Device tree for the Zinwa Q25
+
+```
+#
+# SPDX-FileCopyrightText: The LineageOS Project
+# SPDX-License-Identifier: Apache-2.0
+#
+```
