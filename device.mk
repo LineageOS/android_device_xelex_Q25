acdec34bb9edc8836459a17a704ff62677b2b672
bengris32 <bengris32@protonmail.ch>
2023-07-01 18:06:54 +0700

    Q25: Initial minimised rootdir
    Change-Id: I94815f423f5f2f6dc1c41138428b274df1ce675f
    Signed-off-by: bengris32 <bengris32@protonmail.ch>
    Signed-off-by: Stella Diaz <electi@yzu.moe>
    Signed-off-by: basamaryan <basam.aryan@gmail.com>

--- device.mk
+++ device.mk
@@ -14,6 +14,21 @@ PRODUCT_PACKAGES += \
 # Overlays
 PRODUCT_ENFORCE_RRO_TARGETS := *
 
+# Rootdir
+PRODUCT_PACKAGES += \
+    fstab.mt6789 \
+    fstab.mt6789.ramdisk \
+    init.cgroup.rc \
+    init.connectivity.rc \
+    init.modem.rc \
+    init.mt6789.rc \
+    init.mt6789.usb.rc \
+    init.mtkgki.rc \
+    init.project.rc \
+    init.sensor_2_0.rc \
+    init.stnfc.rc \
+    ueventd.mt6789.rc
+
 # Shipping API level
 PRODUCT_SHIPPING_API_LEVEL := 34
 
