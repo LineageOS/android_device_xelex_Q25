f08e5a388a31d4969e232bbf8f483d8873c62e2d
bengris32 <bengris32@protonmail.ch>
2023-07-01 17:19:00 +0700

    Q25: Configure Kernel image building
    Change-Id: I228403e6f3247b0f025071b1c666a5fcc5904712
    Signed-off-by: bengris32 <bengris32@protonmail.ch>
    Signed-off-by: Stella Diaz <electi@yzu.moe>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -51,6 +51,9 @@ BOARD_MKBOOTIMG_ARGS += --board ""
 
 # Kernel
 BOARD_KERNEL_IMAGE_NAME := Image.gz
+TARGET_KERNEL_ARCH := arm64
+TARGET_KERNEL_SOURCE := kernel/xelex/mt6789
+TARGET_KERNEL_CONFIG := gki_defconfig mgk.config entry_level.config q20_v12_factory.config
 
 # Partitions
 BOARD_FLASH_BLOCK_SIZE := 131072
