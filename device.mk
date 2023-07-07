9d079255f23319dc02a7e92e845093e28bf6eed7
bengris32 <bengris32@protonmail.ch>
2023-07-07 23:10:44 +0700

    Q25: Build MediaTek boot control HAL
    Change-Id: I4f453cfa2c44dcbbd7c611ec55624aff3d7e3d5d
    Signed-off-by: bengris32 <bengris32@protonmail.ch>
    Signed-off-by: Stella Diaz <electi@yzu.moe>

--- device.mk
+++ device.mk
@@ -9,6 +9,11 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/developer_gsi_keys.mk)
 
 # A/B
 PRODUCT_PACKAGES += \
+    android.hardware.boot@1.2-mtkimpl \
+    android.hardware.boot@1.2-mtkimpl.recovery \
+    android.hardware.boot@1.2-service
+
+PRODUCT_PACKAGES += \
     update_engine \
     update_engine_sideload \
     update_verifier
