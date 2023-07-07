31f6994af52eeb7aaed4aa3e71829f40bee57463
bengris32 <bengris32@protonmail.ch>
2023-07-07 22:55:00 +0700

    Q25: Configure dynamic partitions
    Change-Id: I080cb8f7bc29cb0cd6dcb153d22dfbc5c1eb5827
    Signed-off-by: bengris32 <bengris32@protonmail.ch>
    Signed-off-by: Stella Diaz <electi@yzu.moe>

--- device.mk
+++ device.mk
@@ -3,6 +3,13 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+# Dynamic Partitions
+PRODUCT_USE_DYNAMIC_PARTITIONS := true
+PRODUCT_BUILD_SUPER_PARTITION := false
+
+PRODUCT_PACKAGES += \
+    fastbootd
+
 # Shipping API level
 PRODUCT_SHIPPING_API_LEVEL := 34
 
