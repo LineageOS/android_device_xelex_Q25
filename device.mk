33134e9aec088263a0c3f7a07e04c00125d9f4dc
Lokesh Gidra <lokeshgidra@google.com>
2023-07-19 20:19:00 +0100

    Q25: Inherit the Dalvik heap configuration for a 6GB device
    Change-Id: Ib6b1d7ad933fe3867dc6253e229a3c6470135e75
    Signed-off-by: Stella Diaz <electi@yzu.moe>

--- device.mk
+++ device.mk
@@ -6,6 +6,9 @@
 # Installs gsi keys into ramdisk, to boot a developer GSI with verified boot.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/developer_gsi_keys.mk)
 
+# Dalvik VM Configuration
+$(call inherit-product, frameworks/native/build/phone-xhdpi-6144-dalvik-heap.mk)
+
 # Project ID Quota
 $(call inherit-product, $(SRC_TARGET_DIR)/product/emulated_storage.mk)
 
