791b605702c73451772d6fada375ea90b1e86dd6
bengris32 <bengris32@protonmail.ch>
2023-07-24 18:27:29 +0100

    Q25: Configure MediaTek IMS stack
    * Import required system IMS blobs from stock
    * Set the default IMS implementation to com.mediatek.ims via
      overlay.
    * Force enable IMS features via overlay and sysprops.
    * Add MediaTek framework jars to bootjars.
    * Copy priv-app permissions for MediaTek IMS.
    * Pull missing vendor videotelephony implementation.
    
    Change-Id: Ie258bff336586919a1127c3f7241e3ef9d689dd5
    Signed-off-by: bengris32 <bengris32@protonmail.ch>

--- device.mk
+++ device.mk
@@ -94,6 +94,15 @@ PRODUCT_PACKAGES += \
     android.hardware.health-service.mediatek \
     android.hardware.health-service.mediatek-recovery
 
+# IMS
+PRODUCT_BOOT_JARS += \
+    mediatek-common \
+    mediatek-framework \
+    mediatek-ims-base
+
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/privapp-permissions-com.mediatek.ims.xml:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/permissions/privapp-permissions-com.mediatek.ims.xml
+
 # Keylayout
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/idc/Q25_keyboard.idc:$(TARGET_COPY_OUT_VENDOR)/usr/idc/Q25_keyboard.idc \
@@ -118,6 +127,7 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     FrameworksResOverlayQ25 \
     LineageSDKResCommon \
+    TelephonyOverlayQ25 \
     WifiResOverlayQ25
 
 DEVICE_PACKAGE_OVERLAYS += \
