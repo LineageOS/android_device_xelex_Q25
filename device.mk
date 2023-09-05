7f7c6754f7f09f22788651004c29f41187c5e3f5
bengris32 <bengris32@protonmail.ch>
2023-09-05 12:37:49 +0100

    Q25: Switch to AOSP audio service
    Change-Id: I933b66e3821562022eb837af91ad4039e22fb578
    Signed-off-by: bengris32 <bengris32@protonmail.ch>
    Signed-off-by: Stella Diaz <electi@yzu.moe>

--- device.mk
+++ device.mk
@@ -49,7 +49,7 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.audio@7.0-impl \
     android.hardware.audio.effect@7.0-impl \
-    android.hardware.audio.service.mediatek
+    android.hardware.audio.service
 
 PRODUCT_PACKAGES += \
     audio.bluetooth.default \
