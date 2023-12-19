3f54ec66e434fb1a695d4f92d858c21ac15bf3f0
Woomymy <woomy@woomy.be>
2023-12-19 16:09:53 +0000

    Q25: Link media.c2 service against v33 libstagefright_foundation
    * Fixes crashes and high CPU usage
    
    Change-Id: I60c07d10c2db9335d20c6dbaaaeacc9313aebf06
    Signed-off-by: Woomymy <woomy@woomy.be>
    Signed-off-by: Stella Diaz <electi@yzu.moe>

--- extract-files.py
+++ extract-files.py
@@ -118,7 +118,8 @@ blob_fixups: blob_fixups_user_type = {
         'vendor/bin/hw/android.hardware.media.c2@1.2-mediatek',
         'vendor/bin/hw/android.hardware.media.c2@1.2-mediatek-64b',
     ): blob_fixup()
-        .replace_needed('libavservices_minijail_vendor.so', 'libavservices_minijail.so'),
+        .replace_needed('libavservices_minijail_vendor.so', 'libavservices_minijail.so')
+        .add_needed('libstagefright_foundation-v33.so'),
 }  # fmt: skip
 
 module = ExtractUtilsModule(
