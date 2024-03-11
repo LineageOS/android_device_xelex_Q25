1d885aa2ba7a1b984b953e7f3f514d8778582049
Bruno Martins <bgcngm@gmail.com>
2024-03-11 17:23:49 +0100

    Q25: Build common power-libperfmgr and import powerhint
    Change-Id: I22dd0baaf1594db1650bccf37b4d167b50135174
    Co-authored-by: Stella Diaz <electi@yzu.moe>
    Co-authored-by: basamaryan <basam.aryan@gmail.com>
    Signed-off-by: Stella Diaz <electi@yzu.moe>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -145,6 +145,7 @@ TARGET_USERIMAGES_USE_F2FS := true
 ENABLE_VENDOR_RIL_SERVICE := true
 
 # SEPolicy
+include device/lineage/sepolicy/libperfmgr/sepolicy.mk
 include device/mediatek/sepolicy_vndr/SEPolicy.mk
 
 # SPL
