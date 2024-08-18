573b6f0b84bdeff69427fe8cf1256ad89f204343
Aaron Kling <webgeek1234@gmail.com>
2024-08-18 17:05:38 -0500

    Q25: Use v1 rkp interface for keymint
    Change-Id: I281efe89458fefc37a25aa7c2555b9bf92bbdc14

--- extract-files.py
+++ extract-files.py
@@ -100,7 +100,7 @@ blob_fixups: blob_fixups_user_type = {
         .replace_needed('android.hardware.security.keymint-V1-ndk_platform.so', 'android.hardware.security.keymint-V1-ndk.so')
         .replace_needed('android.hardware.security.secureclock-V1-ndk_platform.so', 'android.hardware.security.secureclock-V1-ndk.so')
         .replace_needed('android.hardware.security.sharedsecret-V1-ndk_platform.so', 'android.hardware.security.sharedsecret-V1-ndk.so')
-        .add_needed('android.hardware.security.rkp-V3-ndk.so'),
+        .add_needed('android.hardware.security.rkp-V1-ndk.so'),
 }  # fmt: skip
 
 module = ExtractUtilsModule(
