0aba4dbabdb3e9fbcb30408f077cd814169dedf0
Mashopy <eliasgheeraert@gmail.com>
2025-06-18 17:28:30 +0200

    Q25: Import device specific sensors hals configuration
    Change-Id: Ib4200b4e177994eb757ed301123de3e30d63f851

--- device.mk
+++ device.mk
@@ -225,6 +225,9 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.sensors-service.multihal
 
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/sensors/hals.conf:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/hals.conf
+
 # Shipping API level
 PRODUCT_SHIPPING_API_LEVEL := 34
 
