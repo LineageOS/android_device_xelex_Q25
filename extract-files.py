ddb48eb126d78156e48fb3ae85f220bc6c8b66c7
Stella Diaz <electi@yzu.moe>
2026-02-15 05:14:55 -0500

    Q25: Patch keymint service to load non -platform libs
    Change-Id: I37f4d099fa7057c22aabca8837d9ea935c710bdf
    Signed-off-by: Stella Diaz <electi@yzu.moe>

--- extract-files.py
+++ extract-files.py
@@ -97,6 +97,12 @@ blob_fixups: blob_fixups_user_type = {
         'vendor/bin/hw/android.hardware.memtrack-service.mediatek'
     ): blob_fixup()
         .replace_needed('android.hardware.memtrack-V1-ndk_platform.so', 'android.hardware.memtrack-V1-ndk.so'),
+    (
+        'vendor/bin/hw/android.hardware.security.keymint-service.trustkernel'
+    ): blob_fixup()
+        .replace_needed('android.hardware.security.keymint-V1-ndk_platform.so', 'android.hardware.security.keymint-V1-ndk.so')
+        .replace_needed('android.hardware.security.secureclock-V1-ndk_platform.so', 'android.hardware.security.secureclock-V1-ndk.so')
+        .replace_needed('android.hardware.security.sharedsecret-V1-ndk_platform.so', 'android.hardware.security.sharedsecret-V1-ndk.so'),
 }  # fmt: skip
 
 module = ExtractUtilsModule(
