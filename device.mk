8f85fdb72b19ea651e529b5b4d470a3993944e89
Stella Diaz <electi@yzu.moe>
2026-02-17 05:39:02 -0500

    Q25: Import media_codecs configuration from stock
    Change-Id: Ifc3d1a4143af276e6ccd2dc496a2136613c7da0f
    Signed-off-by: Stella Diaz <electi@yzu.moe>

--- device.mk
+++ device.mk
@@ -103,6 +103,10 @@ PRODUCT_PACKAGES += \
     android.hardware.health@2.1-service
 
 # Media
+PRODUCT_COPY_FILES += \
+    frameworks/av/media/libstagefright/data/media_codecs_google_c2_audio.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_c2_audio.xml \
+    frameworks/av/media/libstagefright/data/media_codecs_google_c2_video.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_c2_video.xml
+
 PRODUCT_PACKAGES += \
     mediacodec.policy \
     mediaextractor.policy \
