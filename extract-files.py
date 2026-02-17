440cc58220f555420db994a1f107e662d22da5ab
Stella Diaz <electi@yzu.moe>
2026-02-16 21:18:13 -0500

    Q25: Initial prop files listing
    * Made using nick's new fangled blob categorizer,
      well first we use a find cmd to generate a txt
      file with vendor/lib and vendor/lib64 files.
      Then we manually add the bin files from vendor/bin
      we know are used ahaha. Run the script and answer
      it's questions. Then go in vendor/etc and manually
      add the files and then figure out why you did all
      this. Next remove all the AOSP built libraries by
      spamming the makefile command and listening to the
      errors. Oh and the mt6789 copy statements are also
      bash command generated using find and sed.
    
    * https://gist.github.com/electimon/72fcea5ad7853cbab13db846741dc2bf
    
    Change-Id: Ief1ad3a85b8b3925cc6a5320dba73704f317e7b5
    Signed-off-by: Stella Diaz <electi@yzu.moe>

--- extract-files.py
+++ extract-files.py
@@ -19,12 +19,6 @@ from extract_utils.main import (
     ExtractUtils,
     ExtractUtilsModule,
 )
-from extract_utils.tools import (
-    llvm_objdump_path,
-)
-from extract_utils.utils import (
-    run_cmd,
-)
 
 namespace_imports = [
     'device/xelex/Q25',
@@ -43,6 +37,69 @@ lib_fixups: lib_fixups_user_type = {
 
 
 blob_fixups: blob_fixups_user_type = {
+    (
+        'vendor/lib64/hw/android.hardware.gnss-impl-mediatek.so',
+        'vendor/bin/hw/android.hardware.gnss-service.mediatek',
+    ): blob_fixup()
+        .replace_needed('android.hardware.gnss-V1-ndk_platform.so', 'android.hardware.gnss-V1-ndk.so'),
+    (
+        'vendor/lib64/android.hardware.power-service-mediatek.so'
+    ): blob_fixup()
+        .replace_needed('android.hardware.power-V2-ndk_platform.so', 'android.hardware.power-V2-ndk.so'),
+    (
+        'vendor/lib64/hw/hwcomposer.mtk_common.so'
+    ): blob_fixup()
+        .add_needed('libprocessgroup_shim.so'),
+    (
+        'vendor/lib64/mt6789/lib3a.flash.so',
+        'vendor/lib64/mt6789/lib3a.ae.stat.so',
+        'vendor/lib64/mt6789/lib3a.sensors.color.so',
+        'vendor/lib64/mt6789/lib3a.sensors.flicker.so',
+    ): blob_fixup()
+        .add_needed('liblog.so'),
+    (
+        'vendor/lib/libGsFace_ca.so',
+        'vendor/lib64/libGsFace_ca.so'
+    ): blob_fixup()
+        .add_needed('libteec.so'),
+    (
+        'vendor/lib64/mt6789/libcam.utils.sensorprovider.so',
+        'vendor/bin/mnld'
+    ): blob_fixup()
+        .replace_needed('libsensorndkbridge.so', 'android.hardware.sensors@1.0-convert-shared.so'),
+    (
+        'vendor/lib64/mt6789/libmnl.so'
+    ): blob_fixup()
+        .add_needed('libcutils.so'),
+    (
+        'vendor/lib/mt6789/libneuralnetworks_sl_driver_mtk_prebuilt.so',
+        'vendor/lib64/mt6789/libneuralnetworks_sl_driver_mtk_prebuilt.so'
+    ): blob_fixup()
+        .clear_symbol_version('AHardwareBuffer_allocate')
+        .clear_symbol_version('AHardwareBuffer_describe')
+        .clear_symbol_version('AHardwareBuffer_lock')
+        .clear_symbol_version('AHardwareBuffer_release')
+        .clear_symbol_version('AHardwareBuffer_unlock')
+        .clear_symbol_version('AHardwareBuffer_createFromHandle')
+        .clear_symbol_version('AHardwareBuffer_getNativeHandle'),
+    (
+        'vendor/lib/libnvram.so',
+        'vendor/lib64/libnvram.so',
+        'vendor/lib/libsysenv.so',
+        'vendor/lib64/libsysenv.so',
+        'vendor/bin/STFlashTool',
+        'vendor/bin/nfcstackp-vendor',
+    ): blob_fixup()
+        .add_needed('libbase_shim.so'),
+    (
+        'vendor/lib/libspeech_enh_lib.so',
+        'vendor/lib64/libspeech_enh_lib.so'
+    ): blob_fixup()
+        .fix_soname(),
+    (
+        'vendor/bin/hw/android.hardware.memtrack-service.mediatek'
+    ): blob_fixup()
+        .replace_needed('android.hardware.memtrack-V1-ndk_platform.so', 'android.hardware.memtrack-V1-ndk.so'),
 }  # fmt: skip
 
 module = ExtractUtilsModule(
