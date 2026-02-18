a3d7964742e0358bd074c91c9425dcc0ffd1f59f
Stella Diaz <electi@yzu.moe>
2026-02-18 12:32:29 -0500

    Q25: Add screen density settings
    Change-Id: I024c9c984e0a51b7c3729a7239f8b53c2c1feecd
    Signed-off-by: Stella Diaz <electi@yzu.moe>

--- device.mk
+++ device.mk
@@ -45,6 +45,10 @@ PRODUCT_PACKAGES += \
     checkpoint_gc \
     otapreopt_script
 
+# AAPT
+PRODUCT_AAPT_CONFIG := normal
+PRODUCT_AAPT_PREF_CONFIG := hdpi
+
 # Audio
 PRODUCT_PACKAGES += \
     android.hardware.audio@7.0-impl \
