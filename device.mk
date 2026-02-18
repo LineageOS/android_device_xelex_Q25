bb7f1685b50add18e56e3e65ef5e8f9d27f9a6cf
Stella Diaz <electi@yzu.moe>
2026-02-17 23:13:13 -0500

    Q25: configs: Import kcm/kl/idc from stock
    Change-Id: I3f3ebbaeaaa3794fb4fd7c0809ffc4157b7e6864
    Signed-off-by: Stella Diaz <electi@yzu.moe>

--- device.mk
+++ device.mk
@@ -94,6 +94,12 @@ PRODUCT_PACKAGES += \
     android.hardware.health-service.mediatek \
     android.hardware.health-service.mediatek-recovery
 
+# Keylayout
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/idc/Q25_keyboard.idc:$(TARGET_COPY_OUT_VENDOR)/usr/idc/Q25_keyboard.idc \
+    $(LOCAL_PATH)/configs/keylayout/Q25_keyboard.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/Q25_keyboard.kl \
+    $(LOCAL_PATH)/configs/keychars/Q25_keyboard.kcm:$(TARGET_COPY_OUT_VENDOR)/usr/keychars/Q25_keyboard.kcm
+
 # Media
 PRODUCT_COPY_FILES += \
     frameworks/av/media/libstagefright/data/media_codecs_google_c2_audio.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_c2_audio.xml \
