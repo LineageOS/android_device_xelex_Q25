5d94352489a3152d6bb27cc3b4fe94bb85697308
Mashopy <eliasgheeraert@gmail.com>
2026-02-24 12:26:20 +0100

    Q25: Inherit telephony-common-stub
    Google deprecated TelephonyMetrics class in Android 16 QPR2 which our IMS stack depends on.
    
    Change-Id: I93ce916aa0ba6d699fade0bc059d8e1a7ac10811

--- device.mk
+++ device.mk
@@ -104,6 +104,8 @@ PRODUCT_PACKAGES += \
     android.hardware.health-service.mediatek-recovery
 
 # IMS
+$(call inherit-product, hardware/lineage/compat/frameworks/compat.mk)
+
 PRODUCT_BOOT_JARS += \
     mediatek-common \
     mediatek-framework \
