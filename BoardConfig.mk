103e67bdc9b72b26a91d86629580b0b1684f0c83
basamaryan <basam.aryan@gmail.com>
2026-03-02 22:22:00 -0800

    Q25: sepolicy: Label wakeup nodes
    Change-Id: Ifd6edd84374480a58ea4c68452ffb36ddf962062

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -134,6 +134,7 @@ ENABLE_VENDOR_RIL_SERVICE := true
 # SEPolicy
 include device/lineage/sepolicy/libperfmgr/sepolicy.mk
 include device/mediatek/sepolicy_vndr/SEPolicy.mk
+BOARD_VENDOR_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
 
 # SPL
 BOOT_SECURITY_PATCH := 2024-03-05
