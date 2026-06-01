54f96e55509148e31ffb2e693582e3567577b5f6
basamaryan <basam.aryan@gmail.com>
2026-06-01 16:07:05 -0700

    Q25: Import default_volume_tables.xml from stock
    Imported as-is for future changes.
    
    Change-Id: I3a84dea49284a5dafd495bed01786b3dac4232cd

--- device.mk
+++ device.mk
@@ -72,6 +72,7 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/audio/audio_effects.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_effects.xml \
     $(LOCAL_PATH)/configs/audio/audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_policy_configuration.xml \
+    $(LOCAL_PATH)/configs/audio/default_volume_tables.xml:$(TARGET_COPY_OUT_VENDOR)/etc/default_volume_tables.xml \
     $(LOCAL_PATH)/configs/audio/usb_audio_accessory_only_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/usb_audio_accessory_only_policy_configuration.xml
 
 PRODUCT_COPY_FILES += \
